663e1e295474987a7073e487673ca5dd22cfe702
ahuggins0006 <ahuggins0006@penguin>
2021-12-10 10:11:11 -0600

    first draft of first part of day3

--- src/aoc_2021/day_3_binary_diagnostic.clj
+++ src/aoc_2021/day_3_binary_diagnostic.clj
@@ -25,21 +25,63 @@
 ;; => ([0 0 1 0 0] [1 1 1 1 0] [1 0 1 1 0] [1 0 1 1 1] [1 0 1 0 1] [0 1 1 1 1] [0 0 1 1 1] [1 1 1 0 0] [1 0 0 0 0] [1 1 0 0 1] [0 0 0 1 0] [0 1 0 1 0])
 
 (def first-col (mapv #(nth % 0) normalized-demo-input));; => (0 1 1 1 1 0 0 1 1 1 0 0)
+(def third-col (mapv #(nth % 2) normalized-demo-input));; => (0 1 1 1 1 0 0 1 1 1 0 0)
 
 
+;; make sure to sort the frequencies as order is not guaranteed
 (defn mcbd [bin-vec]
   ;; most common binary digit
-  (let [zero-or-one (apply compare (vals (frequencies bin-vec)))]
+  (let [zero-or-one (apply compare (vals (sort (frequencies bin-vec))))]
     (case zero-or-one
       -1 1
        1 0)
     ))
+(->> first-col frequencies
+     sort
+     ;vals
+     ;(apply compare)
+     )
+
+(->> third-col frequencies
+     sort
+     ;vals
+     ;(apply compare)
+     )
+
+(defn lcbd [bin-vec]
+  ;; most common binary digit
+  (let [zero-or-one (apply compare (vals (sort (frequencies bin-vec))))]
+    (case zero-or-one
+      -1 0
+      1 1)))
 
 (mcbd first-col)
+(lcbd first-col)
 
-(defn find-gamma-rate [input])
+(defn find-rate [input rate-type]
+  (let [sample-size (count (first input))]
+  (loop [i 0 acc []]
+    (if (>= i sample-size) acc (recur (inc i)
+                                      (conj acc (rate-type (mapv #(nth % i) input))))))))
 
-(loop [x 10]
-  (when (> x 1)
-    (println x)
-    (recur (- x 2))))
+(Integer/parseInt (->> (find-rate normalized-demo-input mcbd) (apply str)) 2)
+;; => 22
+(Integer/parseInt (->> (find-rate normalized-demo-input lcbd) (apply str)) 2)
+;; => 9
+
+(defn calc-power-consumption [input]
+  (let [ gamma-rate   (->> (find-rate input mcbd) (apply str))
+         epsilon-rate (->> (find-rate input lcbd) (apply str))]
+    (reduce * (map #(Integer/parseInt % 2) [gamma-rate epsilon-rate]))))
+
+(calc-power-consumption normalized-demo-input)
+;; => 198
+
+
+;; read input from day 3 file
+
+(def inputs3
+  (line-seq (io/reader(io/resource "day3_input.txt"))))
+
+(calc-power-consumption inputs3)
+;; => 2250414
