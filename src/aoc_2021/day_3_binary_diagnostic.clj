ad28e1e08da7cfd411c0a870b6826803e39a5c59
Andrew Huggins <ahuggins@torchtechnologies.com>
2021-12-15 17:08:29 -0600

    started over since I got off track

--- src/aoc_2021/day_3_binary_diagnostic.clj
+++ src/aoc_2021/day_3_binary_diagnostic.clj
@@ -89,30 +89,7 @@
 
 ;; PART TWO
 
-;; function to filter based on first digit
-
-
-(defn starts-with [decider input]
-  (if (= decider (first input)) input false ))
-
-(starts-with \0 (first demo-input))
-;; => "00100"
-
-(filter #(starts-with \1 %) demo-input);; => ("11110" "10110" "10111" "10101" "11100" "10000" "11001")
-
-(= \a (first "a"))
-
-(defn most-common [pos input] (filter #(starts-with (first (pr-str (mcbd (mapv #(nth % pos) input)))) %) input))
-(most-common demo-input)
-;; => ("11110" "10110" "10111" "10101" "11100" "10000" "11001")
-(filter #(starts-with (first (pr-str (lcbd (mapv first demo-input)))) %) demo-input)
-;; => ("00100" "01111" "00111" "00010" "01010")
-
-
-(defn get-rating [definition input]
-  (let [sample-size (count input)]
-    (loop [i 0 remaining input]
-      (if (>= i sample-size) remaining (recur ;; what to do here?))
-      )
-
-  ))
+normalized-demo-input
+(def a-col (mapv #(nth % 0) normalized-demo-input));; => (0 1 1 1 1 0 0 1 1 1 0 0)
+(mcbd a-col)
+(filter #(= 1 (nth % 1)) normalized-demo-input)
